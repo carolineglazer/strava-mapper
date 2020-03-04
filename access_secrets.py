9ed948e58e65b498322fe938f832877c09d87b59
carolineglazer <carolineroseglazer@gmail.com>
2020-03-04 07:27:25 -0800

    add create_secrets method

--- access_secrets.py
+++ access_secrets.py
@@ -22,3 +22,28 @@ def access_secret_version(project_id, secret_id, version_id):
 	payload = response.payload.data.decode('UTF-8')
 	return payload
 
+def create_secret(project_id, secret_id):
+    """
+    Create a new secret with the given name. A secret is a logical wrapper
+    around a collection of secret versions. Secret versions hold the actual
+    secret material.
+    """
+
+    # Import the Secret Manager client library.
+    from google.cloud import secretmanager_v1beta1 as secretmanager
+
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the parent project.
+    parent = client.project_path(project_id)
+
+    # Create the secret.
+    response = client.create_secret(parent, secret_id, {
+        'replication': {
+            'automatic': {},
+        },
+    })
+
+    # Print the new secret name.
+    print('Created secret: {}'.format(response.name))
